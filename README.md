ead2e5729508bf4ecc57170a765cc469d4d1d13b
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-31 19:41:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Server in C
+
+Created a simple client/server in C.
+
+Usage:
+    $ gcc -o server server.c
+    $ ./server
+
+    $ gcc -o client client.c
+    $ ./client localhost
---
+++ client.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define DATA "Hello Derek!"
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    struct hostent *hp;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("Socket failed");
+        exit(1);
+    }
+
+    server.sin_family = AF_INET;
+
+    hp = gethostbyname(argv[1]);
+    if (hp == 0)
+    {
+        perror("gethostbyname failed.");
+        close(sock);
+        exit(1);
+    }
+
+    memcpy(&server.sin_addr, hp->h_addr, hp->h_length);
+    server.sin_port = htons(5000);
+
+    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        perror("connect failed");
+        close(sock);
+        exit(1);
+    }
+
+    if (send(sock, DATA, sizeof(DATA), 0) < 0)
+    {
+        perror("send failed");
+        close(sock);
+        exit(1);
+    }
+    
+    printf("Sent %s\n", DATA);
+    close(sock);
+
+    return 0;
+}
